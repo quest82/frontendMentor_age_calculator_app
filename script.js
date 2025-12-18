82a8f96b9c3d5cd9f702f2005a1e7af200c16059
quest82 <awoniyioluseyi@gmail.com>
2025-12-18 18:28:13 +0100

    Edited JS Logic to fit and output to the DOM

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
+// DOM Queries
+
+const form = document.querySelector(".ageCalculator__form");
+const yearOutput = document.querySelector(".yearOutput span");
+const monthOutput = document.querySelector(".monthOutput span");
+const dayOutput = document.querySelector(".dayOutput span");
+
 // Store up date of birth in year, month & day as integers
-const yearOfBirth = parseInt("1984", 10);
-const monthOfBirth = parseInt("9", 10);
-const dayOfBirth = parseInt("24", 10);
+const yearOfBirth = parseInt(form.year.value, 10);
+const monthOfBirth = parseInt(form.month.value, 10);
+const dayOfBirth = parseInt(form.day.value, 10);
 
 // Store up the current date in year, month & day as integers
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
-const currentMonth = currentDate.getMonth() + 1; // 1 is added to month of current date to account for Date() indexing
+const currentMonth = 4 //currentDate.getMonth() + 1; // 1 is added to month of current date to account for Date() indexing
 const currentDay = currentDate.getDate();
 
-// Initial calculations to get the age of the user in the years, months and days they've been alive e.g 40 Years, 3 Months, 16 Days
-// Subtracting the current year, month (number), and day from the that of the date of birth should offer the answer easily
-let ageInYears = currentYear - yearOfBirth;
-let ageInMonths = currentMonth - monthOfBirth;
-let ageInDays = currentDay - dayOfBirth;
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-// For when the month of the DOB hasn't been reached (DOB is 6 - June but the current year is 3 - March)
-// This would give a negative using the initial calculation. To account for that, a year needs to be removed from age in years and the deficit month is removed from a full year in months (12)
+	// Initial calculations to get the age of the user in the years, months and days they've been alive e.g 40 Years, 3 Months, 16 Days
+	// Subtracting the current year, month (number), and day from the that of the date of birth should offer the answer easily
+	let ageInYears = currentYear - yearOfBirth;
+	let ageInMonths = currentMonth - monthOfBirth;
+	let ageInDays = currentDay - dayOfBirth;
 
+	// For when the month of the DOB hasn't been reached (DOB is 6 - June but the current year is 3 - March)
+	// This would give a negative month using the initial calculation. To account for that, a year needs to be removed from age in years and the deficit month is removed from a full year in months (12)
 
-if (ageInMonths < 0 && ageInDays < 0) {
-	ageInYears = ageInYears - 1;
-	ageInMonths = 12 + ageInMonths;
-	getDaysWhenNegative();
-}
+	if (ageInMonths < 0 && ageInDays < 0) {
+		getMonthsNegative();
+		getDaysWhenNegative();
+	}
 
-if (ageInDays < 0) {
-	getDaysWhenNegative();
-}
+	//  If the date of the birthday hasn't been reached
+	if (ageInDays < 0) {
+		getDaysWhenNegative();
+	}
 
-if (ageInMonths < 0) {
-	ageInYears = ageInYears - 1;
-	ageInMonths = 12 + ageInMonths;
-}
+	if (ageInMonths < 0) {
+		getMonthsNegative();
+	}
 
-function getDaysWhenNegative() { // If the date of the birthday hasn't been reached, the initial calculation will yield a negative number for age in days so it taken from the full previous month
-	const previousMonthDays = new Date(
-		currentYear,
-		currentMonth - 1,
-		0
-	).getDate(); // To get the number of days of the previous month
-	ageInDays = previousMonthDays + ageInDays;
-	ageInMonths = ageInMonths - 1;
-}
+	yearOutput.textContent = ageInYears
+	monthOutput.textContent = ageInMonths
+	dayOutput.textContent = ageInDays
 
-console.log("Years", ageInYears);
-console.log("Months", ageInMonths);
-console.log("Days", ageInDays);
+	function getMonthsNegative() {
+		ageInYears = ageInYears - 1;
+		ageInMonths = 12 + ageInMonths;
+	}
+
+	function getDaysWhenNegative() {
+		// If the date of the birthday hasn't been reached, the initial calculation will yield a negative number for age in days so it taken from the full previous month
+		const previousMonthDays = new Date(
+			currentYear,
+			currentMonth - 1,
+			0
+		).getDate(); // To get the number of days of the previous month
+		ageInDays = previousMonthDays + ageInDays;
+		ageInMonths = ageInMonths - 1;
+	}
+});
